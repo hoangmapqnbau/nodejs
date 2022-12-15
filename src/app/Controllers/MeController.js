1143835814fd7014241703b3c6b4015c2aa215af
Hoang Nguyen <nguyenhuy.hoang422211@gmail.com>
2022-12-15 21:57:50 +0700

    Actual final build before release.

--- src/app/Controllers/MeController.js
+++ src/app/Controllers/MeController.js
@@ -5,7 +5,8 @@ const { loopToObeject, mongooseToObject } = require('../../utils/mongoose');
 class MeController {
     // [GET] /me 
     show(req, res, next) {
-        Promise.all([User.find({}), User.countDocumentsDeleted()])
+        let userFind =  User.find({});
+        Promise.all([userFind.sortable(req), User.countDocumentsDeleted()])
         .then(([users, numberOfDeleted]) => {
             res.render('user/me',{
                 numberOfDeleted,
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const handlebars = require('express-handlebars');
 const route = require('./routes/index.route');
 const methodOverride = require('method-override')
-
+const sortMiddleware = require('./app/Middlewares/sortMiddleware.js');
 const db = require('./config/database/index');
 db.Connect();
 
@@ -13,10 +13,33 @@ db.Connect();
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(methodOverride('_method'))
+app.use(sortMiddleware);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', handlebars());
+app.engine('handlebars', handlebars({
+    helpers: {
+        sum: function(a, b){return a + b},
+        sortable: function(field, sort){
+                const icons = {
+                    default: 'bi bi-sort-alpha-down',
+                    asc: 'bi bi-sort-alpha-up',
+                    desc: 'bi bi-sort-alpha-down'
+                }
+                
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc'
+                }
+                
+                const icon = icons[sort.type];
+                const type = types[sort.type]
+         
+          return `<a href="?_sort&column=${field}&type=${type}"><span><i class="${icon}"></i></span></a>`;
+        }
+    }
+}));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'resource', 'views'));
 
